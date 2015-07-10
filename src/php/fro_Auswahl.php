33abc14587dac7db307c809d90482b8ef800527a
Standardbenutzer <Standardbenutzer@INR13018>
2015-07-10 10:41:14 +0200

    Auswahldatei erstellt -menüfelder erstellt

--- src/php/fro_Auswahl.php
+++ src/php/fro_Auswahl.php
@@ -8,22 +8,34 @@
              
 
 <body>
-    <table border="1">
+    <br>
+    
+    <table border="1" align="center">
         <tr>
-            <td><a href="fro_hinzufuegen.php">Hinzuf&Uuml;gen</a></td>
-        </tr>
-        <tr>
-            <td><a href="fro_aendern.php">&Auml;ndern</a></td>
-        </tr>
-        <tr>
-            <td><a href="fro_loeschen.php">L&Ouml;schen</a></td>
-        </tr>
+            <td><a href="fro_Hinzufuegen.php">Hinzuf&uuml;gen</a></td>
+            <td><a href="fro_Aendern.php">&Auml;ndern</a></td>      
+            <td><a href="fro_Loeschen.php">L&ouml;schen</a></td>
+        </tr>   
     </table>
     
-    <table>
+    <br><br>
+    
+    <table border="1"> 
         <tr>
-            <td></td>
+            <td><a href="fro_Raeume.php">R&auml;me</a></td>
         </tr>
+        <tr>
+            <td><a href="fro_Lieranten.php">Lieferanten</a></td>
+        </tr>
+        <tr>
+            <td><a href="fro_Geraete.php">Ger&auml;te</a></td>
+        </tr>
+        <tr>
+            <td><a href="fro_Komponenten.php">Komponenten</a></td>
+        </tr>
+        <tr>
+            <td><a href="fro_Arten.php">Arten</a></td>
+        </tr>       
     </table>
 
 
