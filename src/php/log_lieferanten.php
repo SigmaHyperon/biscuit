a62c8f596129ff88f68602b2ca8e91e6c79ab57d
SigmaHyperon <nano_tech@web.de>
2015-07-10 10:36:55 +0200

    lieferanten logic -added leiferanten logic file

---
+++ src/php/log_lieferanten.php
@@ -0,0 +1,17 @@
+<?php
+
+include "sql_main.php";
+include "../lib/manager.php";
+\utility\loadTables();
+
+$aLieferanten = getLieferanten_Data();
+$aLieferanten_Attribute = ["lieferant_id", "lieferant_name"];
+
+$oLieferanten_Tabelle = new \utility\tables\table();
+$oLieferanten_Tabelle->start();
+$oLieferanten_Tabelle->addRow($aRaeume_Attribute);
+foreach ($aLieferanten as $aLieferant)
+{
+    $oLieferanten_Tabelle->addRow($aLieferant);
+}
+$oLieferanten_Tabelle->end();
