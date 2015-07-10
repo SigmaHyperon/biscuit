f86df8511ca476b717a9925a2fdbbf5f81394da8
Standardbenutzer <Standardbenutzer@INR13018>
2015-07-10 09:18:02 +0200

    auswahldatei

---
+++ src/php/fro_Auswahl.php
@@ -0,0 +1,39 @@
+<!DOCTYPE>
+<html >
+
+<head>
+    <title>fro_Auswahl</title>
+
+</head>
+             
+
+<body>
+    <table border="1">
+        <tr>
+            <td><a href="fro_hinzufuegen.php">Hinzuf&Uuml;gen</a></td>
+        </tr>
+        <tr>
+            <td><a href="fro_aendern.php">&Auml;ndern</a></td>
+        </tr>
+        <tr>
+            <td><a href="fro_loeschen.php">L&Ouml;schen</a></td>
+        </tr>
+    </table>
+    
+    <table>
+        <tr>
+            <td></td>
+        </tr>
+    </table>
+
+
+
+    <?php
+    
+    
+    
+    
+    
+    ?>    
+</body>
+</html>
