108cbfa7dd05edde7db0682d6171b686db829c71
Cete1337 <tim.kilian@hotmail.de>
2015-07-13 12:29:39 +0200

    neu

---
+++ src/php/fro_Komponenten_aendern.php
@@ -0,0 +1,20 @@
+<!-----------------------------------------------------------------
+Ersteller:          K!l1an
+Erstell-Datum:      13.07.2015
+Änderungsdatum: 
+Inhalt:             Komponenten hinzufügen
+------------------------------------------------------------------->
+
+<form action="fro_Komponenten_hinzufuegen.php" method="post">
+        <table border="1" cellspacing="10px">
+            <tr>
+                <td width="100px">Name:</td><td><input type="text" name="txt_Komponentenname" size="20"/></td>
+            </tr>
+            <tr>
+                <td >Komponentenbestand:</td><td><input type="text" name="txt_Komponentenbestand" size="20"/></td>
+            </tr>
+            <tr>
+                <td colspan="2" align="center"> <input type="submit" value="Eintragen" /><input type="reset" value="Verwerfen" /></td>
+            </tr>              
+        </table>        
+</form>
