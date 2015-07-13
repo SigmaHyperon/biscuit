a4d5a14b0f51266faa5b8fb3f4a9f4280cff043d
Standardbenutzer <Standardbenutzer@INR13018>
2015-07-13 09:31:35 +0200

    Raumformular erstellt

---
+++ src/php/fro_Raeume_hinzufuegen.php
@@ -0,0 +1,20 @@
+<!-----------------------------------------------------------------
+Ersteller:          Pf3y
+Erstell-Datum:      13.07.2015
+Änderungsdatum: 
+Inhalt:             Räume hinzufügen
+-------------------------------------------------------------------> 
+
+<form action="fro_Raeume_hinzufuegen.php" method="post">
+        <table border="1" cellspacing="10px">
+            <tr>
+                <td width="100px">Name:</td><td><input type="text" name="txt_Raumname" size="20"/></td>
+            </tr>
+            <tr>
+                <td >Raumnotiz:</td><td><input type="text" name="txt_Raumnotiz" size="20"/></td>
+            </tr>
+            <tr>
+                <td colspan="2" align="center"> <input type="submit" value="Eintragen" /><input type="reset" value="Verwerfen" /></td>
+            </tr>              
+        </table>        
+</form>
