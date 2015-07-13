4ef411103fd2a8bbddd673d49421215a28757767
SigmaHyperon <nano_tech@web.de>
2015-07-13 08:06:29 +0200

    added form functionality

--- src/php/fro_Lieferanten_hinzufuegen.php
+++ src/php/fro_Lieferanten_hinzufuegen.php
@@ -14,7 +14,17 @@ Inhalt:             Lieferanten_Formular
                 <td>Name:</td><td><input type="text" name="txt_Name" size="20"/></td>
             </tr>
             <tr>
-                <td colspan="2" align="center"> <input type="reset" value="Verwerfen" /></td>
+                <td colspan="2" align="center"> <input type="submit" value="Abschicken" /><input type="reset" value="Verwerfen" /></td>
             </tr>              
         </table>        
     </form>
+
+<?php
+    require_once '../lib/manager.php';
+    \utility\loadForms();
+    
+    if($s_Lieferant_name = \utility\forms\post("txt_Name", false))
+    {
+	func_form_insertLieferant($txt_lieferant_name);
+    }
+    
