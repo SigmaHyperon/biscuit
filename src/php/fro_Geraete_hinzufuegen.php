39fc22f6d2b2cd0ff453caac6f747735970e7021
SigmaHyperon <nano_tech@web.de>
2015-07-13 12:06:44 +0200

    funct

--- src/php/fro_Geraete_hinzufuegen.php
+++ src/php/fro_Geraete_hinzufuegen.php
@@ -4,7 +4,34 @@ Erstell-Datum:      13.07.2015
 Änderungsdatum: 
 Inhalt:            Geräte hinzufügen
 -------------------------------------------------------------------> 
-
+<?php
+    require_once '../lib/manager.php';
+    require_once './sql_main.php';
+    \utility\loadForms();
+    
+    if($s_Geraet_name = \utility\forms\post("txt_Geraetetyp", false))
+    {
+	$s_Geraet_typ = \utility\forms\post("txt_Vorname", "");
+	$s_Geraet_hersteller = \utility\forms\post("txt_Hersteller", "");
+	$s_Geraet_einkaufsdatum = \utility\forms\post("txt_Datum", "");
+	$s_Geraet_gewaehrleistungsbeginn = \utility\forms\post("txt_Gewaehr_beginn", "");
+	$s_Geraet_gewaehrleistungsende = \utility\forms\post("txt_Gewaehr_ende", "");
+	$s_Geraet_seriennummer = \utility\forms\post("txt_Geraete_seriennummer", "");
+	if(func_form_insertGeraet())
+	{
+	    try {
+		header("Location: fro_Auswahl.php?action=list&table=geraete");
+		die();
+	    } catch (Exception $ex) {}
+	}
+	else 
+	{
+	    echo "An error occured!";
+	}
+    }
+    else
+    {
+?>
 <form action="fro_Geraete_hinzufuegen.php" method="post">
         <table border="1" cellspacing="10px">
             <tr>
@@ -16,6 +43,9 @@ Inhalt:            Geräte hinzufügen
                                             <option>Projektor</option>
                                         </select></td>
             </tr>
+	    <tr>
+		<td>Name:</td><td><input type="text" name="txt_Name" size="20"/></td>
+	    </tr>
             <tr>
                 <td>Hersteller:</td><td><input type="text" name="txt_Hersteller" size="20"/></td>
             </tr>
@@ -36,3 +66,6 @@ Inhalt:            Geräte hinzufügen
             </tr>              
         </table>        
     </form>
+<?php
+    }
+    ?>
