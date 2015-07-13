e7e9f87f18f94ac6ebb211b6cabf471ae48d6e04
Cete1337 <tim.kilian@hotmail.de>
2015-07-13 12:22:58 +0200

    neu

--- src/php/fro_Lieferanten_aendern.php
+++ src/php/fro_Lieferanten_aendern.php
@@ -1,5 +1,5 @@
 <!-----------------------------------------------------------------
-Ersteller:          K!l1an
+Ersteller:          Pf3y
 Erstell-Datum:      12.07.2015
 Änderungsdatum: 
 Inhalt:             Lieferanten_Formular
@@ -9,9 +9,14 @@ Inhalt:             Lieferanten_Formular
     require_once './sql_main.php';
     \utility\loadForms();
     
-    if($s_Lieferant_name = \utility\forms\post("txt_Name", false))
+    if($s_Lieferant_firmenname = \utility\forms\post("txt_Firma", false))
     {
-	if(func_form_insertLieferant($s_Lieferant_name))
+	$s_Lieferant_vorname = \utility\forms\post("txt_Vorname", "");
+	$s_Lieferant_nachname = \utility\forms\post("txt_Name", "");
+	$s_Lieferant_plz = \utility\forms\post("txt_Plz", "");
+	$s_Lieferant_ort = \utility\forms\post("txt_Ort", "");
+	$s_Lieferant_strasse = \utility\forms\post("txt_Strasse", "");
+	if(func_form_insertLieferant($s_Lieferant_firmenname, $s_Lieferant_vorname, $s_Lieferant_nachname, $s_Lieferant_plz, $s_Lieferant_ort, $s_Lieferant_strasse))
 	{
 	    try {
 		header("Location: fro_Auswahl.php?action=list&table=lieferanten");
@@ -44,7 +49,7 @@ Inhalt:             Lieferanten_Formular
                 <td>PLZ:</td><td><input type="text" name="txt_Plz" size="20"/></td>
             </tr>
             <tr>
-                <td>Strasse inkl. Nr.:</td><td><input type="text" name="txt_Plz" size="20"/></td>
+                <td>Strasse inkl. Nr.:</td><td><input type="text" name="txt_Strasse" size="20"/></td>
             </tr>
             <tr>
                 <td colspan="2" align="center"> <input type="submit" value="Eintragen" /><input type="reset" value="Verwerfen" /></td>
