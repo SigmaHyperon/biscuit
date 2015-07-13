432eeeb4b105912e9a2f2500cbf395527f07a66e
SigmaHyperon <nano_tech@web.de>
2015-07-13 08:29:19 +0200

    added form functionality

--- src/php/fro_Lieferanten_hinzufuegen.php
+++ src/php/fro_Lieferanten_hinzufuegen.php
@@ -4,7 +4,28 @@ Erstell-Datum:      12.07.2015
 Änderungsdatum: 
 Inhalt:             Lieferanten_Formular
 ------------------------------------------------------------------->
-
+<?php
+    require_once '../lib/manager.php';
+    require_once './sql_main.php';
+    \utility\loadForms();
+    
+    if($s_Lieferant_name = \utility\forms\post("txt_Name", false))
+    {
+	if(func_form_insertLieferant($s_Lieferant_name))
+	{
+	    try {
+		header("Location: fro_Auswahl.php?action=list&table=lieferanten");
+		die();
+	    } catch (Exception $ex) {}
+	}
+	else 
+	{
+	    echo "An error occured!";
+	}
+    }
+    else
+    {
+	?>
     <form action="fro_Lieferanten_hinzufuegen.php" method="post">
         <table border="1">
             <tr>
@@ -18,14 +39,8 @@ Inhalt:             Lieferanten_Formular
             </tr>              
         </table>        
     </form>
-
 <?php
-    require_once '../lib/manager.php';
-    require_once './sql_main.php';
-    \utility\loadForms();
-    
-    if($s_Lieferant_name = \utility\forms\post("txt_Name", false))
-    {
-	func_form_insertLieferant($s_Lieferant_name);
     }
+    ?>
+
     
