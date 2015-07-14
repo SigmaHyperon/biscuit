f32efc0b9d5c0584fc0278a5e0fdda331e697405
SigmaHyperon <nano_tech@web.de>
2015-07-14 12:30:57 +0200

    added id

--- src/php/fro_Komponenten_aendern.php
+++ src/php/fro_Komponenten_aendern.php
@@ -11,11 +11,11 @@ Inhalt:             Komponenten ändern
     
     if($s_Komponente_name = \utility\forms\post("txt_Komponentenname", false))
     {
-	$s_Komponente_art = \utility\forms\post("txt_Komponentenart_select", "");
+	$s_Komponente_id = \utility\forms\post("int_id", "");
 	$s_Komponente_art = \utility\forms\post("txt_Komponentenart_select", "");
 	$i_Komponente_bestand = \utility\forms\post("txt_Komponentenbestand", "");
 	
-	if(func_form_updateKomponenten($s_Komponente_name, $i_Komponente_bestand, $s_Komponente_art))
+	if(func_form_updateKomponenten($s_Komponente_id, $s_Komponente_name, $i_Komponente_bestand, $s_Komponente_art))
 	{
 	    try {
 		header("Location: fro_Auswahl.php?action=list&table=komponenten");
@@ -38,11 +38,11 @@ Inhalt:             Komponenten ändern
 
         <table border="1" class="formular">
             <tr>
-                <td width="100px">Name:</td><td><input type="text" name="txt_Komponentenname" size="20" value="<?php echo $aKomponenten_daten["komponenten_id"];?>"/></td>
+                <td width="100px">Name:</td><td><input type="text" name="txt_Komponentenname" size="20" value="<?php echo $aKomponenten_daten["komponente_name"];?>"/></td>
 
             </tr>
             <tr>
-                <td >Komponentenbestand:</td><td><input type="text" name="txt_Komponentenbestand" size="20" value="<?php echo $aKomponenten_daten["komponenten_id"];?>"/></td>
+                <td >Komponentenbestand:</td><td><input type="text" name="txt_Komponentenbestand" size="20" value="<?php echo $aKomponenten_daten["komponente_bestand"];?>"/></td>
             </tr>
             <tr>
                 <td>Komponentenart:</td>
@@ -52,7 +52,7 @@ Inhalt:             Komponenten ändern
 			    $aAlle_komponenten_arten = func_a_getKomponentenArten();
 			    foreach ($aAlle_komponenten_arten as $value) {
 				echo "<option " ;
-				if($value["komponenten_art_id"] == $aGeraet_daten["komponenten_art_fk"]) echo "selected";
+				if($value["komponenten_art_id"] == $aKomponenten_daten["komponenten_art_fk"]) echo "selected";
 				echo " value='".$value["komponenten_art_id"]."'>".$value["komponenten_art_name"]."</option>";
 			    }
 			?>
