f4fb1cfc59b81f7e5e1757f22901053b2effccf3
E-Motion <E-Motion@Black_Shift>
2015-07-14 11:43:07 +0200

    Auslagern von Funktionen in versch. Dateien.
    Korrigieren von func del komponente

---
+++ src/php/sql_insert.php
@@ -0,0 +1,149 @@
+<?php
+
+
+/**
+ * 
+ * @param type $txt_lieferant_firmenname - Name Firma
+ * @param type $txt_lieferant_vorname - Vorname Kontaktperson
+ * @param type $txt_lieferant_nachname - Nachname Kontaktperson
+ * @param type $_int_lieferant_plz - PLZ
+ * @param type $txt_lieferant_ort - Ort Lieferant
+ * @param type $txt_lieferant_strasse - Straße Lieferant
+ * @return Gibt zurück ob Eintragen erfolgreich (int)
+ */
+function func_form_insertLieferant($txt_lieferant_firmenname, $txt_lieferant_vorname, $txt_lieferant_nachname, $int_lieferant_plz, $txt_lieferant_ort, $txt_lieferant_strasse)
+{
+    $txt_sql_statement = "INSERT INTO tbl_lieferanten (lieferant_firmenname, 
+                                                                             lieferant_vorname,
+                                                                             lieferant_nachname,
+                                                                             lieferant_plz, 
+                                                                             lieferant_ort, 
+                                                                             lieferant_strasse)
+                                                            VALUES ('".$txt_lieferant_firmenname."',
+                                                                           '".$txt_lieferant_vorname."',
+                                                                           '".$txt_lieferant_nachname."',
+                                                                           '".$int_lieferant_plz."',
+                                                                           '".$txt_lieferant_ort."',
+                                                                           '".$txt_lieferant_strasse."');";
+    
+    $int_response = mysql_query($txt_sql_statement);
+    var_dump($txt_sql_statement);
+    return($int_response);
+}
+
+/**
+ * 
+ * @param type $txt_raum_name - Der Name des Raumes
+ * @param type $txt_raum_notiz - Die Raumnotiz
+ * @return Gibt Information über Erfolg d. Eintragens (Int)
+ */
+function func_form_insertRaum($txt_raum_name, $txt_raum_notiz)
+{
+    $txt_sql_statement = "INSERT INTO tbl_raeume (raum_name, raum_notiz)
+                            VALUES ('".$txt_raum_name."','".$txt_raum_notiz."');";
+    
+    $int_response = mysql_query($txt_sql_statement);
+    return($int_response);
+}
+
+/**
+ * 
+ * @param type $int_raum - Raum Gerät
+ * @param type $int_lieferant - Lieferant Gerät
+ * @param type $dat_ek_datum - Einkaufsdatum Gerät
+ * @param type $txt_notiz -Der Gerät wird nicht müde
+ * @param type $txt_hersteller - Der Gerät schläft nie
+ * @param type $dat_gewaehr_beginn - Der Gerät macht nicht schlapp
+ * @param type $dat_gewaehr_ende - Gewähr Ende
+ * @param type $txt_seriennummer - Seriennummer Gerät
+ * @param type $int_art - Typenbeschreibung / art
+ * @param type $text_name Name des Gerätes
+ * @return Gibt an ob der Eintrag erfolgreich war (int)
+ */
+function func_form_insertGeraet($int_raum, $int_lieferant, $dat_ek_datum, $txt_notiz, $txt_hersteller, $dat_gewaehr_beginn, $dat_gewaehr_ende, $txt_seriennummer, $int_art, $text_name)
+{
+    $txt_sql_statement = "INSERT INTO tbl_geraete (raum_fk,
+                                                                        lieferant_fk,
+                                                                        geraet_ek_datum,
+                                                                        geraet_notiz,
+                                                                        geraet_hersteller,
+                                                                        geraet_gewaehr_beginn,
+                                                                        geraet_gewaehr_ende,
+                                                                        geraete_seriennummer,
+                                                                        geraete_art_fk,
+                                                                        geraet_name)
+                                                       VALUES ('".$int_raum."',
+                                                                    '".$int_lieferant."',
+                                                                    '".$dat_ek_datum."',
+                                                                    '".$txt_notiz."',
+                                                                    '".$txt_hersteller."',
+                                                                    '".$dat_gewaehr_beginn."',
+                                                                    '".$dat_gewaehr_ende."',
+                                                                    '".$txt_seriennummer."',
+                                                                    '".$int_art."',
+                                                                    '".$text_name."');";
+    $int_response = mysql_query($txt_sql_statement);
+    return($int_response);
+}
+
+/**
+ * 
+ * @param type $txt_name - Komponentenname
+ * @param type $int_anzahl - Menge d. Komponenten
+ * @return Gibt Information über Erfolg d. Eintragens (Int)
+ */
+function func_form_insertKomponente($txt_name, $int_anzahl,$int_art_fk)
+{
+    $txt_sql_statemnet = "INSERT INTO tbl_komponenten (komponente_name, komponente_bestand, komponenten_art_fk)
+                                    VALUES ('".$txt_name."',
+                                                 '".$int_anzahl."',
+                                                 '".$int_art_fk."');";
+    
+    $int_response = mysql_query($txt_sql_statemnet);
+    return ($int_response);
+    
+}
+
+/**
+ * 
+ * @param type $txt_name - Name der Art
+ * @return Gibt Information über Erfolg d. Eintragens (Int)
+ */
+function func_form_instertKomponentenArt($txt_name)
+{
+    $txt_sql_statement = "INSERT INTO tbl_komponenten_arten (komponenten_art_name)
+                            VALUES (".$txt_name.");";
+    
+    $int_response = mysql_query($txt_sql_statement);
+    return ($int_response);
+}
+
+/**
+ * 
+ * @param type $txt_name - Name des Attributs
+ * @return Gibt Information über Erfolg d. Eintragens (Int)
+ */
+function func_form_instertAttribut($txt_name)
+{
+    $txt_sql_statement = "INSERT INTO tbl_komponenten_attribute (koponenten_attribut_name)
+                            VALUES (".$txt_name.");";
+    
+    $int_response = mysql_query($txt_sql_statement);
+    return($int_response);
+}
+
+/**
+ * 
+ * @param type $txt_name - Name des Wertes
+ * @param type $int_wert - Der eigentliche Wert
+ * @return Gibt Information über Erfolg d. Eintragens (Int)
+ */
+function func_form_instertZulaessigenWert($txt_name, $int_wert)
+{
+    $txt_sql_statement = "INSERT INTO tbl_zulaessige_Werte (zulaessiger_wert_name, zulaessiger_wert) 
+                                     VALUES (".$txt_name."),
+                                   (".$int_wert.");";                       
+    
+    $int_response = mysql_query($txt_sql_statement);
+    return($response);
+}
---
+++ src/php/sql_update.php
@@ -0,0 +1,89 @@
+<?php
+
+
+function func_b_updateRaum ($int_id, $s_name, $s_notiz)
+{
+    $txt_sql_statement = "UPDATE tbl_raeume set raum_name='".$s_name."', raum_notiz='".$s_notiz."' where raum_id='".$int_id."';";    
+    var_dump($txt_sql_statement);
+    $a_sql_result =  mysql_query($txt_sql_statement)
+            or die ("Anfrage Fehlgeschlagen");
+
+    return $a_sql_result;
+}
+
+function func_b_updateLieferant ($int_id, $s_firmenname, $s_vorname, $s_nachname, $s_plz, $s_ort, $s_strasse)
+{
+    $txt_sql_statement = "UPDATE tbl_lieferanten set lieferant_firmenname='".$s_firmenname."'"
+	    . ", lieferant_vorname='".$s_vorname."'"
+	    . ", lieferant_nachname='".$s_nachname."'"
+	    . ", lieferant_plz='".$s_plz."'"
+	    . ", lieferant_ort='".$s_ort."'"
+	    . ", lieferant_strasse='".$s_strasse."' where lieferant_id='".$int_id."';";    
+    var_dump($txt_sql_statement);
+    $a_sql_result =  mysql_query($txt_sql_statement)
+            or die ("Anfrage Fehlgeschlagen");
+
+    return $a_sql_result;
+}
+
+function func_form_updateGeraet($int_id, $int_raum, $int_lieferant, $dat_ek_datum, $txt_notiz, $txt_hersteller, $dat_gewaehr_beginn, $dat_gewaehr_ende, $txt_seriennummer, $int_art, $text_name)
+{
+    $txt_sql_statement = "UPDATE tbl_geraete SET raum_fk='".$int_raum."',
+						lieferant_fk='".$int_lieferant."',
+						geraet_ek_datum='".$dat_ek_datum."',
+						geraet_notiz='".$txt_notiz."',
+						geraet_hersteller='".$txt_hersteller."',
+						geraet_gewaehr_beginn='".$dat_gewaehr_beginn."',
+						geraet_gewaehr_ende='".$dat_gewaehr_ende."',
+						geraete_seriennummer='".$txt_seriennummer."',
+						geraete_art_fk='".$int_art."',
+						geraet_name='".$text_name."'
+						WHERE geraete_id='".$int_id."'";
+    $int_response = mysql_query($txt_sql_statement);
+    return($int_response);
+}
+
+function func_form_updateKomponenten($int_id, $txt_komponente_name, $int_komponente_bestand, $int_komponenten_art)
+{
+    $txt_sql_statement = "UPDATE tbl_komponenten SET koponente_name = '".$txt_komponente_name."',
+                                                                                komponente_bestand = '".$int_komponente_bestand."',
+                                                                                komponente_art_fk ='".$int_komponenten_art."',
+                                                                     WHERE komponenten_id = '".$int_id."'";
+    $int_response = mysql_query($txt_sql_statement);
+    return($int_response);
+}
+
+function func_form_updateKomponentenArt($int_id, $txt_name)
+{
+    $txt_sql_statement = "UPDATE tbl_komponenten_arten SET komponenten_art_name = '".$txt_name."'
+                                                                             WHERE komponenten_id = '".$int_id."'";
+    $int_response = mysql_query($txt_sql_statement);
+    return($int_response);
+}
+
+function func_form_updateAttribute($int_id, $txt_name)
+{
+    $txt_sql_statement = "UPDATE tbl_komponenten_Attribute SET komponenten_attribut_name ='".$txt_name."'
+                                                                                  WHERE komponenten_:attribut_id ='".$int_id."'";
+    $int_response = mysql_query($txt_sql_statement);
+    return($int_response);
+}
+
+function func_form_updateZulaessigeWerte($int_id, $txt_wert_name, $int_wert)
+{
+    $txt_sql_statement = "UPDATE tbl_zulaessige_werte SET zulaessiger_wert_name ='".$txt_wert_name."',
+                                                                                    zulaessiger_wert = '".$int_wert."'
+                                                                         WHERE zulaessiger_wert_id ='".$int_id."'";
+    $int_response = mysql_query($txt_sql_statement);
+    return($int_response);
+}
+
+function func_form_updateBenutzer($int_id, $txt_name)
+{
+    $txt_sql_statement = "UPDATE tbl_benutzer SET benutzer_name = '".$txt_name."'
+                                                              WHERE benutzer_id ='".$int_id."'";
+    $int_response = mysql_query($txt_sql_statement);
+    return($int_response);
+}
+
+
