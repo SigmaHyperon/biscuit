acb58c99576230e51d8c0039311e00600da92487
Pf3y <paddy.fey@gmx.de>
2015-07-14 14:40:23 +0200

    passwort typ geändert

--- src/php/fro_Home.php
+++ src/php/fro_Home.php
@@ -7,7 +7,7 @@
                 <td>Benutzer:</td><td><input type="text" name="txt_Benutzer"/></td>
             </tr>
             <tr>
-                <td>Passwort</td><td><input type="text" name="txt_Passwort"/></td>
+                <td>Passwort</td><td><input type="password" name="txt_Passwort"/></td>
             </tr>
             <tr>
                 <td colspan="2" align="center"> <input type="submit" value="Login" /><input type="reset" value="Verwerfen" /></td>
