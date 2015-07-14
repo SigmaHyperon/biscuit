af484d170557598996b17a09dff6062d4558d12a
SigmaHyperon <nano_tech@web.de>
2015-07-14 21:34:54 +0200

    enhanced

--- src/php/fro_Home.php
+++ src/php/fro_Home.php
@@ -1,29 +1,30 @@
 
 <?PHP 
-
-if ($_SESSION["login"] != 1)
+if(isset($_SESSION["login"]))
 {
-echo "<div id='logindiv'>
-    <form action='fro_Home.php' method='post'>
-        <table class='login'>
-            <tr>
-                <td>Benutzer:</td><td><input type='text' name='txt_Benutzer'/></td>
-            </tr>
-            <tr>
-                <td>Passwort</td><td><input type='password' name='txt_Passwort'/></td>
-            </tr>
-            <tr>
-                <td colspan='2' align='center'> <input type='submit' value='Login' /><input type='reset' value='Verwerfen' /></td>
-            </tr>              
-        </table>        
-    </form>
-</div>";
+    if ($_SESSION["login"] != 1)
+    {
+    ?>
+    <div id='logindiv'>
+	<form action='fro_Home.php' method='post'>
+	    <table class='login'>
+		<tr>
+		    <td>Benutzer:</td><td><input type='text' name='txt_Benutzer'/></td>
+		</tr>
+		<tr>
+		    <td>Passwort</td><td><input type='password' name='txt_Passwort'/></td>
+		</tr>
+		<tr>
+		    <td colspan='2' align='center'> <input type='submit' value='Login' /><input type='reset' value='Verwerfen' /></td>
+		</tr>              
+	    </table>        
+	</form>
+    </div>
+    <?php
+    }
 }
 
-elseif ($_SESSION["login"] == 1) 
-    {
-        
-    }
+
 
 
 
