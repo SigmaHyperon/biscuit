7d8048ed34ede5d57a6995b2369f1abf0bb21a09
SigmaHyperon <nano_tech@web.de>
2015-07-15 19:06:42 +0200

    added index

---
+++ src/php/index.php
@@ -0,0 +1,12 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+try {
+    header("Location: fro_Auswahl.php?action=home");
+    die();
+} catch (Exception $ex) {}
