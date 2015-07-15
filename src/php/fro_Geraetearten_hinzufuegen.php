d697956167c8bdc22b016f0aa415fce2b53ed9f6
Pf3y <paddy.fey@gmx.de>
2015-07-15 10:00:16 +0200

    geräte/arten_ändern/hinzufügen und main.css überarbeitet

--- src/php/fro_Geraetearten_hinzufuegen.php
+++ src/php/fro_Geraetearten_hinzufuegen.php
@@ -26,6 +26,7 @@ Inhalt:            Gerätearten ändern
     else
     {
 ?>
+<h2 align="center">Gerätearten</h2>
 <form action="fro_Geraetearten_hinzufuegen.php" method="post">
         <table  class="formular">
             <tr>
