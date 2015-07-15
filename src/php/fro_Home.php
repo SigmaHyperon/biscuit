4cf5e56b6fa63b71949a5f33c864dc82792c62b8
SigmaHyperon <nano_tech@web.de>
2015-07-15 07:56:16 +0200

    fixed

--- src/php/fro_Home.php
+++ src/php/fro_Home.php
@@ -1,31 +1,43 @@
 
 <?PHP 
+function showLogin()
+{
+        ?>
+	<div id='logindiv'>
+	    <form action='fro_Home.php' method='post'>
+		<table class='login'>
+		    <tr>
+			<td>Benutzer:</td><td><input type='text' name='txt_Benutzer'/></td>
+		    </tr>
+		    <tr>
+			<td>Passwort</td><td><input type='password' name='txt_Passwort'/></td>
+		    </tr>
+		    <tr>
+			<td colspan='2' align='center'> <input type='submit' value='Login' /><input type='reset' value='Verwerfen' /></td>
+		    </tr>              
+		</table>        
+	    </form>
+	</div>
+    <?php
+}
 if(isset($_SESSION))
 {
-    if ($_SESSION["login"] != 1)
+    if (isset($_SESSION["login"]))
     {
-    ?>
-    <div id='logindiv'>
-	<form action='fro_Home.php' method='post'>
-	    <table class='login'>
-		<tr>
-		    <td>Benutzer:</td><td><input type='text' name='txt_Benutzer'/></td>
-		</tr>
-		<tr>
-		    <td>Passwort</td><td><input type='password' name='txt_Passwort'/></td>
-		</tr>
-		<tr>
-		    <td colspan='2' align='center'> <input type='submit' value='Login' /><input type='reset' value='Verwerfen' /></td>
-		</tr>              
-	    </table>        
-	</form>
-    </div>
-    <?php
+	if($_SESSION["login"] != 1)
+	{
+	    showLogin();
+	}
+	else
+	{
+	    echo 'Hier sollte etwas stehen';
+	}
     }
     else
     {
-    echo 'Hier sollte etwas stehen';
+	showLogin();
     }
+    
 }
 
     
